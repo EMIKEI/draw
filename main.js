80186ccd4e672c9a8149d2331f4b59362e0b44eb
EMIKEI <72548507+EMIKEI@users.noreply.github.com>
2021-07-03 12:14:11 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+const requestBar = document.querySelector(".requestBar")
+const requestSubmit = document.querySelector(".requestSubmit")
+const boxZone = document.querySelector(".boxZone")
+const box_explain = document.querySelector(".box_explain")
+
+{
+    requestSubmit.addEventListener("click", function(){
+        if(requestBar.value === ""){
+            alert("입력해주시길 바랍니다.")
+        }else{
+            boxZone.classList.remove("hide");
+            box_explain.innerHTML = `네가 원하는 "${requestBar.value}"은(는) 이 상자 안에 들어있어. `;
+            requestBar.value = null ;
+            window.scrollBy({
+                top: boxZone.offsetHeight/2.15,
+                behavior: "smooth"
+            })
+        }
+    })
+}
+
+{
+    const closeBut = document.querySelector(".closeBut")
+    
+    closeBut.addEventListener("click", function(){
+        boxZone.classList.add("hide")
+    })
+}
